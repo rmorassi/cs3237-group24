baac9b7f5f5df78c68f4541a0d0124cda97a8320
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-10-18 14:01:38 +0800

    Changed to MQTT

--- comms/test_client.py
+++ comms/test_client.py
@@ -1,10 +1,14 @@
 import socketio
+import base64 as b64
 
 # Change this to the address that appears on start up of the server
 # the 127 address is a loopback so if connecting from a separate device choose
 # a different one.
-IP = '10.14.0.2'
+IP = '192.168.54.92'
 PORT = '5000'
+
+with open("/mnt/c/Users/mrx20/Downloads/animal_shoes.jpeg", "rb") as image_file:
+    TEST_IMAGE = b64.b64encode(image_file.read())
 
 sio = socketio.Client()
 
@@ -15,3 +19,4 @@ def print_message(message: str):
 if __name__ == '__main__':
     sio.connect(f'http://{IP}:{PORT}')
     sio.emit('message', 'Hi, this is my message!')
+    sio.emit('image', TEST_IMAGE)
