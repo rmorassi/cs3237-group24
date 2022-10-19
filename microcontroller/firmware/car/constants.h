5e3769ed69f330ea8c411180b6c0ad4dae09d536
Roberto <s2026885@ed.ac.uk>
2022-10-19 23:11:07 +0800

    car firmware

---
+++ microcontroller/firmware/car/constants.h
@@ -0,0 +1,25 @@
+/* Information for wireless network */
+#define SSID "POCO F3"
+#define PASSWORD "boboWifi"
+
+/* Information for MQTT communications */
+#define BROKER "some.ip.address"
+#define PORT 1883
+// Topics
+#define DIRECTION_RAW_TOPIC "car/direction/raw"
+#define DIRECTION_GAIN_TOPIC "car/direction/gain"
+#define SPEED_TOPIC "car/speed"
+#define CONTROL_TOPIC "car/control"
+
+/* Information I/O */
+// Left Motor connections (A)
+#define L_SPE D0    // ENA
+#define L_FOR D1    // IN1
+#define L_REV D2    // IN2
+// Right Motor connections (B)
+#define R_SPE D3    // ENB
+#define R_FOR D4    // IN3
+#define R_REV D5    // IN4
+
+/* Vehicle dynamics */
+#define DIRECTION_GAIN_MULTIPLIER 2
