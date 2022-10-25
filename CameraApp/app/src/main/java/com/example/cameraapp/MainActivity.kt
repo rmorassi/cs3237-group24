2f6ef229a2d31fe7f863b58fff2b91447a034bfe
Joshua Chew <joshuagchew@gmail.com>
2022-10-25 17:30:50 +0800

    add mainactivity code

--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.cameraapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO ask for permission of camera upon first launch of application
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            ) {
+                val permission = arrayOf(
+                    Manifest.permission.CAMERA
+
+                )
+                requestPermissions(permission, 1122)
+            } else {
+                //TODO show live camera footage
+
+            }
+        }else{
+            //TODO show live camera footage
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //TODO show live camera footage
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //TODO show live camera footage
+        } else {
+            finish()
+        }
     }
 }
