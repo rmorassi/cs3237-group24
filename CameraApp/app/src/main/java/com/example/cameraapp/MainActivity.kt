e50afd46077dd811a35c6ddad50ffefcde9fda40
Joshua Chew <joshuagchew@gmail.com>
2022-10-26 23:54:17 +0800

    live camera feed shown in app - need to implement frame capture and communication

--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.cameraapp
 
 import android.Manifest
+import android.app.Fragment
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.media.ImageReader
+import android.media.ImageReader.OnImageAvailableListener
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import android.view.Surface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnImageAvailableListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO ask for permission of camera upon first launch of application
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
             ) {
@@ -21,22 +28,65 @@ class MainActivity : AppCompatActivity() {
                 )
                 requestPermissions(permission, 1122)
             } else {
-                //TODO show live camera footage
+                setFragment()
 
             }
         }else{
-            //TODO show live camera footage
+            setFragment()
 
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //TODO show live camera footage
         if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            //TODO show live camera footage
+            setFragment()
         } else {
             finish()
         }
+    }
+
+    var previewHeight = 0
+    var previewWidth = 0
+    var sensorOrientation = 0
+    protected fun setFragment() {
+        val manager =
+            getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        var cameraId: String? = null
+        try {
+            cameraId = manager.cameraIdList[0]
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        val fragment: Fragment
+        val camera2Fragment = CameraConnectionFragment.newInstance(
+            object :
+                CameraConnectionFragment.ConnectionCallback {
+                override fun onPreviewSizeChosen(size: Size?, rotation: Int) {
+                    previewHeight = size!!.height
+                    previewWidth = size.width
+                    sensorOrientation = rotation - getScreenOrientation()
+                }
+            },
+            this,
+            R.layout.camera_fragment,
+            // TODO: HAVE CONTROL PARAMETERS
+            Size(640, 480)
+        )
+        camera2Fragment.setCamera(cameraId)
+        fragment = camera2Fragment
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+    }
+    protected fun getScreenOrientation(): Int {
+        return when (windowManager.defaultDisplay.rotation) {
+            Surface.ROTATION_270 -> 270
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_90 -> 90
+            else -> 0
+        }
+    }
+
+    override fun onImageAvailable(reader: ImageReader?) {
+        reader?.acquireLatestImage()?.close()
     }
 }
