70e2a6f91e8f6f3d383933ae9afdfe371707323a
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-11-05 01:09:01 +0800

    MQTT messages can be sent/received

--- notification-app/suitcase-notifications/App.js
+++ notification-app/suitcase-notifications/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const IP = '123.456.789.0'; // Update this
+const IP = '172.31.215.122'; // Update this
 const PORT = 9001; // Check this
 
 client = new Paho.Client(IP, PORT, 'notification-app');
@@ -15,14 +15,25 @@ export default function App() {
     client.connect({
       onSuccess: () => {
         console.log('Connected!');
-        client.subscribe('notifications');
+        client.subscribe('notifications', {
+          onSuccess: () => console.log('subscribed'),
+        });
         client.onMessageArrived = onMessage;
+        // client.onMessageDelivered = () => console.log('Hi');
+        client.onConnectionLost = () => console.log('lost connection');
       },
       onFailure: () => {
         console.log('Failed to connect!');
       },
     });
   }, []);
+
+  const onMessage = (message) => {
+    // if (message.destinationName === 'notifications') {
+    console.log(`Message received: ${message.payloadString}`);
+    client.publish('ack', 'message received!');
+    // }
+  };
 
   return (
     <View style={styles.container}>
