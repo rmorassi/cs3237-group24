be0037124750067e5585ab6d40483e62fc12c3eb
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-11-07 18:40:00 +0800

    Attempting to receive images and display them

--- comms/test_client.py
+++ comms/test_client.py
@@ -1,23 +1,38 @@
 import base64 as b64
+import paho.mqtt.client as mqtt
+import time
 
 # This file is outdated and needs to be updated to use MQTT
 
 # Change this to the address that appears on start up of the server
 # the 127 address is a loopback so if connecting from a separate device choose
 # a different one.
-IP = '192.168.54.92'
-PORT = '5000'
+IP = '172.27.210.101'
+PORT = 1883
+PUB_TOPIC = 'notifications'
 
 with open("/mnt/c/Users/mrx20/Downloads/animal_shoes.jpeg", "rb") as image_file:
     TEST_IMAGE = b64.b64encode(image_file.read())
 
-sio = socketio.Client()
+def main():
+    client = setup()
+    for i in range(12):
+        send_image(client)
+        time.sleep(3)
+    
+    print("Done!")
 
-@sio.on('message')
+def setup():
+    client = mqtt.Client()
+    client.connect(IP, 1883, 60)
+    time.sleep(2)
+    return client
+
+def send_image(client):
+    client.publish(PUB_TOPIC, TEST_IMAGE)
+
 def print_message(message: str):
     print(f"Received message: {message}")
 
 if __name__ == '__main__':
-    sio.connect(f'http://{IP}:{PORT}')
-    sio.emit('message', 'Hi, this is my message!')
-    sio.emit('image', TEST_IMAGE)
+    main()
